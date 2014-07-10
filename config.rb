4cb0b782ca97241eb691fc1540392ceb1ba666c7
Clay Allsopp <clay.allsopp@gmail.com>
2014-07-10 15:00:36 -0700

    brand new design!

--- config.rb
+++ config.rb
@@ -6,9 +6,21 @@ module ToolboxHelpers
       end
     end
   end
+
+  def tags
+    @tags ||= categories.map {|c|
+      c['wrappers'].map {|w|
+        w['tags']
+      }
+    }.flatten.uniq.sort_by(&:downcase)
+  end
 end
 
 helpers ToolboxHelpers
+
+configure :development do
+  activate :livereload
+end
 
 # Build-specific configuration
 configure :build do
--- source/assets/js/app.js
+++ source/assets/js/app.js
@@ -1,71 +1,102 @@
 $(function () {
 
-  var tags = [];
-  var tagContainer = $('div.tag-search');
+  var tags = window.tags;
+  var tagContainer = $('.filter-tags table');
 
-  $('div.wrapper-tag').each( function() {
-    var txt = $(this).text().trim();
+  var currentQuery = {
+    tags: [],
+    platforms: []
+  };
 
-    if ( $.inArray( txt, tags ) === -1 ) {
-      // Addit to an internal array
-      tags.push(txt);
-    }
-  });
+  var filter = function(query){
+    var filtered = $('.wrapper-info').filter(function(){
+      var platformMatch = false;
+      var tagMatch = false;
 
-  tags = tags.sort(function(a, b) {
-    return a.toLowerCase() > b.toLowerCase();
-  }); // Sort 'em!
+      if (query.platforms.length > 0) {
+        platformMatch = $(this).find('.platform').filter(function(){
+          var thisPlatform = $(this).text().replace(",", "").trim();
+          var matchesQuery = query.platforms.indexOf(thisPlatform) !== -1;
+          return matchesQuery
+        }).size() === query.platforms.length;
+      }
+      else {
+        platformMatch = true;
+      }
 
-  // Append them to the dom
-  for (var tag in tags) {
-    tagContainer.append('<div class="wrapper-tag">' + tags[tag] + '</div>');
+      if (query.tags.length > 0) {
+        tagMatch = $(this).find('td.wrapper-tag').filter(function(){
+          var thisValue = $(this).text().trim();
+          return query.tags.indexOf(thisValue) !== -1;
+        }).size() === query.tags.length;
+      }
+      else {
+        tagMatch = true;
+      }
+
+      return platformMatch && tagMatch;
+    });
+
+    // Show the wrappers and then immediately hide the ones that contain the filter tag
+    $('.wrapper-info').show().not(filtered).hide();
+
+    // Hide category labels if there are no results in them
+    /*
+    $(".category-info").show().each(function() {
+      var $el = $(this);
+      var $wrapperList = $el.next();
+      if ($(".wrapper-info:visible", $wrapperList).size() === 0) {
+        $el.hide();
+      }
+    });
+    */
+
+    $(".wrappers-column-1").removeClass("adjusted");
+    $(".wrappers-column-0").show();
+    $(".wrappers-list").each(function() {
+      if ($(".wrappers-column-0 .wrapper-info:visible", this).size() === 0) {
+        $(".wrappers-column-0", this).hide();
+        $(".wrappers-column-1", this).addClass("adjusted");
+      }
+    });
   }
 
-  tagContainer.on('click', 'div.wrapper-tag', function(e){
-    var selected = $(this);
-    var selectedText = selected.text();
 
-    if(selected.hasClass('active')) {
-      selected.removeClass('active');
-      filter(null);
-    } else {
-      // Set the active class on the selected
-      $('div.wrapper-tag').removeClass('active');
-      selected.addClass('active');
-      filter(selectedText);
+  var onFilterClick = function(queryArray) {
+    return function(e) {
+      var selected = $(this);
+      var selectedText = selected.text();
+
+      if(selected.hasClass('active')) {
+        var index = queryArray.indexOf(selectedText);
+        queryArray.splice(index, 1);
+        selected.removeClass('active');
+        filter(currentQuery);
+      } else {
+        // Set the active class on the selected
+        queryArray.push(selectedText);
+        selected.addClass('active');
+        filter(currentQuery);
+      }
+    }
+  };
+
+  tagContainer.on('click', 'td', onFilterClick(currentQuery.tags));
+
+  $(".filter-platforms table").on('click', 'td', onFilterClick(currentQuery.platforms));
+
+  $(".wrapper-tags-list").hide();
+  $(".wrapper-tags-label").click(function(ev) {
+    ev.preventDefault();
+    var $el = $(this);
+    if ($el.hasClass("active")) {
+      $el.removeClass("active");
+      $(".wrapper-tags-list", $el.parent()).hide();
+    }
+    else {
+      $el.addClass("active");
+      $(".wrapper-tags-list", $el.parent()).show();
     }
   });
 
 });
-
-function filter(tag){
-  if(tag === null){
-    $('div.wrapper-info, div.clear').show();
-    return;
-  }
-  filtered = $('div.wrapper-info').filter(function(){
-    show = false;
-    $(this).find('div.wrapper-tag').each(function(){
-      if($(this).text().trim() == tag) {
-        show = true;
-        return;
-      }
-    });
-    return show;
-  });
-
-  // Show all sections:
-  $('div.category-info').show();
-
-  // Show the wrappers and then immediately hide the ones that contain the filter tag
-  $('div.wrapper-info').show().not(filtered).hide();
-  $('div.clear').hide();
-
-  //Hide the sections that have no visible elements
-  // COmmenting out till I can get it working.
-  // $('div.category-info').each(function(){
-  //   console.log($(this).text());
-
-  //   console.log($('div :not(category-info)', this).next(':visible').children(':visible'));
-  // });
-}
